4e9451b3b522b181dfa324ac5bdff733af96aead
MouseDotExecutable <blasermichael3@gmail.com>
2024-06-24 21:28:21 +0200

    Update 22

--- scenes/ending/ending_screen.gd
+++ scenes/ending/ending_screen.gd
@@ -3,4 +3,4 @@ extends CanvasLayer
 
 func _input(event):
 	if Input.is_action_just_pressed("interact") or Input.is_action_just_pressed("ui_accept"):
-		pass
+		get_tree().call_deferred("change_scene_to_file", "res://scenes/loading_screen/loading_screen3.tscn")
